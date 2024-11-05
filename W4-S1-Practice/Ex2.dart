3b6e283798780542a4166e8dbd8a6050f77fa8cb
nith <hengpanharanith099@gmail.com>
2024-11-06 00:55:26 +0700

    Nith

--- W4-S1-Practice/Ex2.dart
+++ W4-S1-Practice/Ex2.dart
@@ -1,31 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Products"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: Product.values.map((product) {
-              return ProductCard(
-                product: product,
-              );
-            }).toList(),
-          ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Products"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: Product.values.map((product) {
+            return ProductCard(
+              product: product,
+            );
+          }).toList(),
         ),
       ),
-    );
-  }
+    ),
+  ));
 }
 
 enum Product {
