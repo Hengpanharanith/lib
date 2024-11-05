44254038fb55c07e9c37a48a966121b48df75c39
nith <hengpanharanith099@gmail.com>
2024-11-06 02:36:28 +0700

    Nith

--- W4 - S3 Stateful Widget - PRACTICE/EX-1/ex_1_start.dart
+++ W4 - S3 Stateful Widget - PRACTICE/EX-1/ex_1_start.dart
@@ -1,20 +1,70 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Custom buttons"),
-        ),
-        body: Center(
-          child: SizedBox(
-                width: 400,
-                height: 100,
-                child: ElevatedButton(
-                    onPressed: () => {},
-                    child: const Center(
-                      child: Text("Not Selected"),
-                    )),
-              ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Click This"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(100.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SelectionButton(),
+              SizedBox(height: 20.0),
+              SelectionButton(),
+              SizedBox(height: 20.0),
+              SelectionButton(),
+              SizedBox(height: 20.0),
+              SelectionButton(),
+            ],
+          ),
         ),
       ),
-    ));
+    )));
+
+class SelectionButton extends StatefulWidget {
+  @override
+  _SelectionButtonState createState() => _SelectionButtonState();
+}
+
+class _SelectionButtonState extends State<SelectionButton> {
+  bool isSelected = false;
+
+  void toggleSelection() {
+    setState(() {
+      isSelected = !isSelected;
+    });
+  }
+
+  String get buttonText => isSelected ? "Selected" : "Not selected";
+
+  Color get textColor => isSelected ? Colors.white : Colors.black;
+
+  Color get backgroundColor =>
+      isSelected ? Colors.blue[500]! : Colors.blue[50]!;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      height: 100,
+      child: ElevatedButton(
+        onPressed: toggleSelection,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
