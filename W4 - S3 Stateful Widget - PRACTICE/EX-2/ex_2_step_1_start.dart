d536c0c2b14bc037a16d5513c4dbc1a3e6ec8687
nith <hengpanharanith099@gmail.com>
2024-11-06 02:39:07 +0700

    Nith

--- W4 - S3 Stateful Widget - PRACTICE/EX-2/ex_2_step_1_start.dart
+++ W4 - S3 Stateful Widget - PRACTICE/EX-2/ex_2_step_1_start.dart
@@ -7,12 +7,76 @@ void main() => runApp(MaterialApp(
           backgroundColor: Colors.blue,
           title: const Text("Favorite cards"),
         ),
-        body: Column(
+        body: const Column(
           children: [
-            Container(
-                // YOUR CODE
-                )
+            FavoriteCard(isFavorite: true),
+            FavoriteCard(isFavorite: false),
+            FavoriteCard(isFavorite: true),
           ],
         ),
       ),
     ));
+
+class FavoriteCard extends StatefulWidget {
+  final bool isFavorite;
+
+  const FavoriteCard({Key? key, required this.isFavorite}) : super(key: key);
+
+  @override
+  FavoriteCardState createState() => FavoriteCardState();
+}
+
+class FavoriteCardState extends State<FavoriteCard> {
+  late bool isFavorite;
+
+  @override
+  void initState() {
+    super.initState();
+    isFavorite = widget.isFavorite;
+  }
+
+  void toggleFavorite() {
+    setState(() {
+      isFavorite = !isFavorite;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 0.5, color: Colors.grey),
+        ),
+      ),
+      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Expanded(
+            flex: 7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'title',
+                  style: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.w800),
+                ),
+                SizedBox(height: 5.0),
+                Text('description')
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: toggleFavorite,
+            icon: Icon(
+              Icons.favorite,
+              color: isFavorite ? Colors.red : Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
