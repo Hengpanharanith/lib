2185e0705e5ba9024fae9295b0ee596042c6035b
nith <hengpanharanith099@gmail.com>
2024-11-17 23:56:06 +0700

    Nith

--- W6-S1/EXERCISE-4/jokes.dart
+++ W6-S1/EXERCISE-4/jokes.dart
@@ -18,4 +18,72 @@ List<Jokes> jokes = [
     title: "Why was the math book sad?",
     content: "Because it had too many problems.",
   ),
+  Jokes(
+    title: "Why did the scarecrow win an award?",
+    content: "Because he was outstanding in his field!",
+  ),
+  Jokes(
+    title: "Why don't eggs tell jokes?",
+    content: "They might crack up.",
+  ),
+  Jokes(
+    title: "Why couldn't the bicycle stand up by itself?",
+    content: "It was two tired.",
+  ),
+  Jokes(
+    title: "What do you call cheese that isn't yours?",
+    content: "Nacho cheese!",
+  ),
+  Jokes(
+    title: "Why did the golfer bring two pairs of pants?",
+    content: "In case he got a hole in one.",
+  ),
+  Jokes(
+    title: "What do you call fake spaghetti?",
+    content: "An impasta!",
+  ),
+  Jokes(
+    title: "Why was the computer cold?",
+    content: "It left its Windows open.",
+  ),
+  Jokes(
+    title: "Why can't your nose be 12 inches long?",
+    content: "Because then it would be a foot!",
+  ),
+  Jokes(
+    title: "Why did the tomato turn red?",
+    content: "Because it saw the salad dressing!",
+  ),
+  Jokes(
+    title: "Why don’t scientists trust atoms?",
+    content: "Because they make up everything!",
+  ),
+  Jokes(
+    title: "What do you call an alligator in a vest?",
+    content: "An investigator.",
+  ),
+  Jokes(
+    title: "Why did the coffee file a police report?",
+    content: "It got mugged.",
+  ),
+  Jokes(
+    title: "What do you get when you cross a snowman with a vampire?",
+    content: "Frostbite.",
+  ),
+  Jokes(
+    title: "Why was the stadium so cool?",
+    content: "Because it was full of fans!",
+  ),
+  Jokes(
+    title: "Why do seagulls fly over the ocean?",
+    content: "Because if they flew over the bay, they’d be bagels.",
+  ),
+  Jokes(
+    title: "Why don't oysters share their pearls?",
+    content: "Because they’re shellfish.",
+  ),
+  Jokes(
+    title: "Why did the skeleton go to the party alone?",
+    content: "He had no body to go with him.",
+  ),
 ];
