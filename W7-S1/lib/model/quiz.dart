94815dd224294899c7470cae72bc5f797f116b17
nith <hengpanharanith099@gmail.com>
2024-11-23 23:57:09 +0700

    Nith

--- W7-S1/lib/model/quiz.dart
+++ W7-S1/lib/model/quiz.dart
@@ -1,15 +1,49 @@
- 
-class Quiz {
-  final String title;
-  final List<Question> questions;
+class Question {
+  final String text;
+  final List<String> options;
+  final String correctAnswer;
 
-  Quiz({required this.title, required this.questions});
+  Question({
+    required this.text,
+    required this.options,
+    required this.correctAnswer,
+  });
 }
 
-class Question {
-  final String title;
-  final List<String> possibleAnswers;
-  final String goodAnswer;
+class Quiz {
+  final List<Question> _questions = [
+    Question(
+      text: 'What is Flutter?',
+      options: ['A framework', 'A language', 'An IDE', 'A database'],
+      correctAnswer: 'A framework',
+    ),
+    Question(
+      text: 'Who created Flutter?',
+      options: ['Apple', 'Google', 'Microsoft', 'Facebook'],
+      correctAnswer: 'Google',
+    ),
+  ];
 
-  const Question({required this.title, required this.possibleAnswers, required this.goodAnswer});
+  int _currentIndex = 0;
+
+  Question? getCurrentQuestion() {
+    if (_currentIndex < _questions.length) {
+      return _questions[_currentIndex];
+    }
+    return null;
+  }
+
+  void nextQuestion() {
+    if (!isLastQuestion()) {
+      _currentIndex++;
+    }
+  }
+
+  bool isLastQuestion() => _currentIndex >= _questions.length - 1;
+
+  void reset() {
+    _currentIndex = 0;
+  }
+
+  List<Question> get questions => _questions;
 }
